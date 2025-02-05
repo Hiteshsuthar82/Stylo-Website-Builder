c7f221ad7f17baf650516fd52a6dee87d1b91830
Hiteshsuthar82 <hiteshsuthar8255@gmail.com>
2025-02-04 20:09:22 -0800

    using hardcoded value insted of env variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    '/api':import.meta.env.VITE_API_URL
+    '/api':'https://stylo-website-builder.onrender.com'
   },
   plugins: [react()],
 })
